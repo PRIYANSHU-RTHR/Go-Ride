dbc9f2ca2ac41923f44bfc1df022ce8a7f7b2e88
PRIYANSHU-RTHR <priyanshurathore403@gmail.com>
2026-06-18 00:48:51 +0530

    feat: publish first message

--- services/trip-service/internal/infrastructure/grpc/grpc_handler.go
+++ services/trip-service/internal/infrastructure/grpc/grpc_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/services/trip-service/internal/infrastructure/events"
 	pb "ride-sharing/shared/proto/trip"
 	"ride-sharing/shared/types"
 
@@ -15,12 +16,14 @@ import (
 type gRPCHandler struct {
 	pb.UnimplementedTripServiceServer
 
-	service domain.TripService
+	service   domain.TripService
+	publisher *events.TripEventPublisher
 }
 
-func NewGRPCHandler(server *grpc.Server, service domain.TripService) *gRPCHandler {
+func NewGRPCHandler(server *grpc.Server, service domain.TripService, publisher *events.TripEventPublisher) *gRPCHandler {
 	handler := &gRPCHandler{
-		service: service,
+		service:   service,
+		publisher: publisher,
 	}
 
 	pb.RegisterTripServiceServer(server, handler)
@@ -41,8 +44,9 @@ func (h *gRPCHandler) CreateTrip(ctx context.Context, req *pb.CreateTripRequest)
 		return nil, status.Errorf(codes.Internal, "failed to create the trip: %v", err)
 	}
 
-	// Add a comment at the end of the function to publish an event on the Async Comms module.
-
+	if err := h.publisher.PublishTripCreated(ctx); err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to publish the trip created event: %v", err)
+	}
 	return &pb.CreateTripResponse{
 		TripID: trip.ID.Hex(),
 	}, nil
@@ -71,7 +75,7 @@ func (h *gRPCHandler) PreviewTrip(ctx context.Context, req *pb.PreviewTripReques
 
 	estimatedFares := h.service.EstimatePackagesPriceWithRoute(route)
 
-	fares, err := h.service.GenerateTripFares(ctx, estimatedFares, userID,route)
+	fares, err := h.service.GenerateTripFares(ctx, estimatedFares, userID, route)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to generate the ride fares: %v", err)
 	}
--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -1,6 +1,8 @@
 package messaging
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -8,30 +10,86 @@ import (
 )
 
 type RabbitMQ struct {
-	conn *amqp.Connection
-}
-
-func connect(uri string) (*amqp.Connection, error) {
-	return amqp.Dial(uri)
+	conn    *amqp.Connection
+	Channel *amqp.Channel
 }
 
 func NewRabbitMQ(uri string) (*RabbitMQ, error) {
 	const retryDelay = 5 * time.Second
 
+	var (
+		conn *amqp.Connection
+		err  error
+	)
+
 	for {
-		conn, err := connect(uri)
+		conn, err = amqp.Dial(uri)
 		if err == nil {
-			return &RabbitMQ{
-				conn: conn,
-			}, nil
+			break
 		}
 
 		log.Printf("Waiting for RabbitMQ: %v", err)
 		time.Sleep(retryDelay)
 	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("failed to create channel: %w", err)
+	}
+
+	rmq := &RabbitMQ{
+		conn:    conn,
+		Channel: ch,
+	}
+
+	if err := rmq.setupExchangesAndQueues(); err != nil {
+		rmq.Close()
+		return nil, fmt.Errorf("failed to setup exchanges and queues: %w", err)
+	}
+
+	log.Println("Connected to RabbitMQ")
+
+	return rmq, nil
 }
+
+func (r *RabbitMQ) PublishMessage(
+	ctx context.Context,
+	routingKey string,
+	message string,
+) error {
+	return r.Channel.PublishWithContext(
+		ctx,
+		"",         // default exchange
+		routingKey, // queue name
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+}
+
+func (r *RabbitMQ) setupExchangesAndQueues() error {
+	_, err := r.Channel.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // auto-delete
+		false,   // exclusive
+		false,   // no-wait
+		nil,
+	)
+
+	return err
+}
+
 func (r *RabbitMQ) Close() {
+	if r.Channel != nil {
+		_ = r.Channel.Close()
+	}
+
 	if r.conn != nil {
-		r.conn.Close()
+		_ = r.conn.Close()
 	}
 }
